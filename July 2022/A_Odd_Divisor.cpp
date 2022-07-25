be5e2aa714413519690e54110655ae1fefe18e89
deep_aman <deepamansinghads@gmail.com>
2022-07-25 23:39:50 +0530

    25-07-2022

---
+++ July 2022/A_Odd_Divisor.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        if (n & 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            while (n % 2 == 0)
+            {
+                n /= 2;
+            }
+            if (n > 1)
+            {
+                cout << "YES" << endl;
+            }
+            else
+            {
+                cout << "NO" << endl;
+            }
+        }
+    }
+    return 0;
+}
