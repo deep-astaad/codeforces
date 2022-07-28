223bf3907aa4ac7ed2f2d7578294342818cd59ef
deep_aman <deepamansinghads@gmail.com>
2022-07-28 23:56:50 +0530

    28-07-2022

---
+++ July 2022/excitingBets.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long a,b;
+        cin>>a>>b;
+        if (a==b)
+        {
+            cout<<0<<" "<<0<<endl;
+        }
+        else if (a<b)
+        {
+            long long temp = b-a;
+            cout<<temp<<" "<<min(a%temp,temp-(a%temp))<<endl;
+        }
+        else{
+            long long temp = a-b;
+            cout<<temp<<" "<<min(a%temp,temp-(a%temp))<<endl;
+        }
+        
+    }
+    return 0;
+}
