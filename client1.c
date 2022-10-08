be14340d120b6dafa471cb12ad8c6c3045346a06
deep_aman <deepamansinghads@gmail.com>
2022-10-08 16:07:17 +0530

    Till 08-10-2022

--- client1.c
+++ client1.c
@@ -11,7 +11,7 @@ int main(int argc, char const* argv[])
 {
 	int sock = 0, valread, client_fd;
 	struct sockaddr_in serv_addr;
-	char* hello = "i am IIIT Nagpur student";
+	char* hello = "I am Aman Deep";
 	char buffer[1024] = { 0 };
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		printf("\n Socket creation error \n");
